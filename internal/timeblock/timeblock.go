33574ed328f852b59d21c4d48f6a7e8d329ff0a2
thinkty <thinkty@naver.com>
2022-04-18 16:32:23 +0900

    Added valid bit to timeblock struct, implemented method to remove block from the list, implemented check method to go over the blocks in the list to see if the block has expired yet

--- internal/timeblock/timeblock.go
+++ internal/timeblock/timeblock.go
@@ -10,6 +10,7 @@ type Timeblock struct {
 	Url       string    `json:"URL"`
 	Duration  uint16    `json:"Duration"` // in 30 minutes (0 ~ 1460)
 	StartTime time.Time `json:"StartTime"`
+	Valid     bool      `json:"Valid"`
 }
 
 func (timeblock Timeblock) String() string {
@@ -17,6 +18,7 @@ func (timeblock Timeblock) String() string {
 	result += fmt.Sprintf("\turl:\t%s\n", timeblock.Url)
 	result += fmt.Sprintf("\tduration:\t%.2f hrs\n", float32(timeblock.Duration)/2)
 	result += fmt.Sprintf("\tstartTime:\t%s\n", timeblock.StartTime)
+	result += fmt.Sprintf("\tValid:\t%v\n", timeblock.Valid)
 
 	return result
 }
@@ -29,11 +31,11 @@ type Timeblocks struct {
 
 // Add a new timeblock to the list if it has a unique url
 func (timeblocks *Timeblocks) Add(url string, duration uint16, startTime time.Time) (Timeblock, error) {
-	newTimeblock := Timeblock{Url: url, Duration: duration, StartTime: startTime}
+	newTimeblock := Timeblock{Url: url, Duration: duration, StartTime: startTime, Valid: true}
 
 	// Check if url is unique
 	for _, timeblock := range timeblocks.Blocks {
-		if url == timeblock.Url {
+		if timeblock.Valid && url == timeblock.Url {
 			return timeblock, errors.New("The given URL already exists in the list")
 		}
 	}
@@ -48,20 +50,41 @@ func (timeblocks *Timeblocks) AddNow(url string, duration uint16) (Timeblock, er
 	return timeblocks.Add(url, duration, time.Now())
 }
 
-// Remove the timeblock with the given url from the list if it exists
-func (timeblocks *Timeblocks) Remove(url string) {
-	// TODO:
+// Remove the timeblock with the given url from the list if it exists.
+// Return true if removal was successful.
+func (timeblocks *Timeblocks) Remove(url string) bool {
+	for _, timeblock := range timeblocks.Blocks {
+		if timeblock.Valid && timeblock.Url == url {
+			timeblock.Valid = false
+			timeblocks.Length--
+			return true
+		}
+	}
 
-	timeblocks.Length--
+	return false
 }
 
 // Go through the list and return urls that are active
 func (timeblocks *Timeblocks) Check() []string {
 	activeUrls := make([]string, 0)
 
-	// TODO:
 	for _, timeblock := range timeblocks.Blocks {
-		activeUrls = append(activeUrls, timeblock.Url)
+		// Skip invalid timeblocks
+		if !timeblock.Valid {
+			continue
+		}
+
+		endTime := timeblock.StartTime.Add(30 * time.Minute * time.Duration(timeblock.Duration))
+
+		// Compare each timeblock's (duration + start time) to (the current time)
+		if time.Now().After(endTime) {
+			timeblock.Valid = false
+			timeblocks.Length--
+
+		} else {
+			// Append active timeblock's url to the list
+			activeUrls = append(activeUrls, timeblock.Url)
+		}
 	}
 
 	return activeUrls
