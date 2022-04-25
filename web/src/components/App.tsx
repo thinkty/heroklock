4717a344daff70cf1a5eceb7a76b9d586c766b52
thinkty <thinkty@naver.com>
2022-04-25 17:57:09 +0900

    Added fetch function for adding new timeblock

--- web/src/components/App.tsx
+++ web/src/components/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { fireEvent, Event } from '../util';
 import { Footer } from './Footer';
 import { Form } from './Form';
+import { TimeblockType } from './Timeblock';
+import { TimeblockContainer } from './TimeblockContainer';
 
 type Props = {
   message: string;
@@ -23,14 +26,27 @@ export const App = (props: Props): JSX.Element => {
         fontFamily: 'Verdana, sans-serif',
         backgroundColor: 'black',
         color: 'white',
+        gap: 20,
       }}
     >
       <h1>
         { props.message }
       </h1>
       <Form
-        onSubmit={(url, dur) => { console.log(url, dur) }}
+        onSubmit={(url, dur): void => {
+          fetch('/add?url=' + url + '&duration=' + dur)
+            .then((res) => res.json())
+            .then((data: TimeblockType) => {
+              // Add new timeblock to container
+              fireEvent(Event.NewBlock, data);
+            })
+            .catch((err) => {
+              console.error(err);
+              alert('Failed to add new timeblock!');
+            });
+        }}
       />
+      <TimeblockContainer />
       <Footer />
     </div>
   );
